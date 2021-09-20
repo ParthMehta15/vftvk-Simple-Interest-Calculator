540d621bd85737f2801bc440e9cba0c2c2d64af6
ParthMehta15 <46846270+ParthMehta15@users.noreply.github.com>
2021-09-20 23:29:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ function compute()
         "in the year <mark>" + year + "</mark>";
 }
 
+function compute_compound()
+{
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var amount = (principal) * ((1+rate) ** years);
+    var com_interest = amount-principal
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="Compound Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + com_interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
+}
+
+
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
